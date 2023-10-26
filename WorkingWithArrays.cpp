7447513b95d2ae07175619a7c1f70edd52980dd5
Vitaly Panev <panevvitalij@gmail.com>
2023-10-26 11:52:03 +0300

    Added a two-dimensional array

--- WorkingWithArrays.cpp
+++ WorkingWithArrays.cpp
@@ -1,12 +1,31 @@
 ﻿
 #include <iostream>
 
-int TwoDimensionalArray()
+void TwoDimensionalArray()
 {
+	const int size = 5;
+	int array[size][size];
 
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++) 
+		{
+			array[i][j] = i + j; 
+		}
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{			
+			std::cout << array[i][j];			
+		}
+		std::cout << '\n';
+	}
 }
 
 int main()
 {
-    
+	TwoDimensionalArray();
+	return 0;
 }
