faa45862ca71081dc94cc8a5062b85b6c9cc18dc
Vitaly Panev <panevvitalij@gmail.com>
2023-10-30 10:04:43 +0300

    Added day, index and index sum

--- WorkingWithArrays.cpp
+++ WorkingWithArrays.cpp
@@ -1,5 +1,6 @@
 ﻿
 #include <iostream>
+#include <ctime>
 
 void TwoDimensionalArray()
 {
@@ -22,6 +23,25 @@ void TwoDimensionalArray()
 		}
 		std::cout << '\n';
 	}
+
+	struct tm newtime;
+	time_t now = time(0);
+	localtime_s(&newtime, &now);
+	int Day = newtime.tm_mday;
+	std::cout << "Day: " << Day << "\n";
+
+	int index = Day % size;
+	std::cout << "index: " << index << "\n";
+	
+	int sum = 0;
+	for (int i = 0; i < size; ++i) 
+	{
+			sum += array[index][i];
+		
+	}
+	std::cout << "Sum: " << sum;
+
+	
 }
 
 int main()
